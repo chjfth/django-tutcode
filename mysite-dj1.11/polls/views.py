1b26107c309c5b0701f5a4d65cf01ae69d7fed55
chj <chjfth@gmail.com>
2018-07-01 00:53:16 +0800

    Add more tests from tut05: * Test a view (test IndexView) * Testing the DetailView

--- mysite-dj1.11/polls/views.py
+++ mysite-dj1.11/polls/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.urls  import reverse # tut04
 from django.views import generic # tut04
+from django.utils import timezone
 
 # Create your views here.
 
@@ -74,20 +75,33 @@ def results_ngv(request, question_id): # tut04: non-generic view
 # https://docs.djangoproject.com/en/1.11/intro/tutorial04/#use-generic-views-less-code-is-better
 #
 class IndexView(generic.ListView):
-    template_name = 'polls/index.html'
-    context_object_name = 'latest_question_list'
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_question_list'
 
-    def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+	def get_queryset_buggy_from_tut04(self):
+		"""Return the last five published questions."""
+		return Question.objects.order_by('-pub_date')[:5]
+	 
+	def get_queryset(self): # corrected from tut05
+		"""Return the last five published questions (not including those set to be
+		published in the future)."""
+		return Question.objects.filter(
+			pub_date__lte=timezone.now()
+		).order_by('-pub_date')[:5]
+
 
 class DetailView(generic.DetailView):
-    model = Question
-    template_name = 'polls/detail.html'
+	model = Question
+	template_name = 'polls/detail.html'
+
+	def get_queryset(self):
+		"""Excludes any questions that aren't published yet."""
+		return Question.objects.filter(pub_date__lte=timezone.now())
+
 
 class ResultsView(generic.DetailView):
-    model = Question
-    template_name = 'polls/results.html'
+	model = Question
+	template_name = 'polls/results.html'
 
 
 
